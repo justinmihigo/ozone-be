d5ebcbfc1b93389ed45ab160361cff1e491af468
justinmihigo <mihigojustin28@gmail.com>
2025-08-08 14:44:34 +0200

    ch: add remote url for the db

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,13 @@ import { PackageModule } from './packages/package.module';
 import { InclusionModule } from './inclusions/inclusion.module';
 import { UserModule } from './users/user.module';
 import { EmailModule } from './email/email.module';
+import {configDotenv} from 'dotenv';
+configDotenv();
+const MONGO_URI= process.env.MONGO_URI || 'mongodb://localhost:27017/ozone-travel';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/ozone-travel', ),
+    MongooseModule.forRoot(MONGO_URI),
     PackageModule,
     InclusionModule,
     UserModule,
